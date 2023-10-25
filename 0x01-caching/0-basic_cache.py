8c35ff3806ae0e5fe73a43a8358bb64411b4ca0f
NebihaDilsebo <shegolenur@hotmail.com>
2023-10-25 03:34:05 -0700

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" BasicCache class that inherits from BaseCaching """
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """ A caching system that doesn't have a limit """
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        return self.cache_data.get(key, None)
